b25f520685994026af61fc72d3f6875b39579ac1
陈毅 <ychenronron@163.com>
2024-04-02 20:25:59 +0800

    feat:前台分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import ZhyfSwiper from '@/components/ZhyfSwiper.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { getHomeBannerApi } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem } from '@/types/home'
 import { ref } from 'vue'
 // 保存轮播图的数据
-const BannerList = ref<BannerItemrItem[]>([])
+const BannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerApi()
   // (property) Data<unknown>.result: unknown
   BannerList.value = res.result
 }
-// 页面加载的时候
+// 页面加载的时候 onLoad加载
 onLoad(() => {
   getHomeBannerData()
 })
@@ -24,10 +25,14 @@ onLoad(() => {
     <CustomNavbar />
     <!-- 轮播图 -->
     <ZhyfSwiper :list="BannerList" />
-    <view class="index">hello</view>
+    <!-- 分类面板 -->
+    <CategoryPanel />
+    <view class="index"></view>
   </div>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
